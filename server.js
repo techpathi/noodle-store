1addc760dc2bf071b405a1e67a2f60dfd3d4733b
techpathi <venkat4coding@gmail.com>
2021-04-03 14:17:19 +0530

    Heroku depoloy fix

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const path = require("path");
 const express = require("express");
 const app = express();
-app.use(express.static(__dirname + 'dist/noodle-store'));
+app.use(express.static(__dirname + '/noodle-store'));
 app.get('/*', function(req,res){
-res.sendFile(path.join(__dirname, 'dist/noodle-store', 'index.html'))
+res.sendFile(path.join(__dirname, 'noodle-store', 'index.html'))
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
